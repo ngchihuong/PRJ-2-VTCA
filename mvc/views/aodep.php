4917e1bb70a26fa71e94a41eecfaae03dac5ffbe
huynk2101 <140798340+huynk2101@users.noreply.github.com>
2024-01-08 16:15:54 +0700

    update

---
+++ mvc/views/aodep.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        div {
+            padding: 20px;
+        }
+        #header , #footer {
+            background-color: yellow;
+        }
+    </style>
+</head>
+<body>
+   <div id="header"></div>
+    <div id="content">
+
+        <img src="https://th.bing.com/th/id/OIP.BIZbOHp-Enot1H-jcLKYJgHaFX?w=220&h=181&c=7&r=0&o=5&dpr=1.3&pid=1.7"
+            alt="logo">
+    </div>
+    <div id="footer"></div>
+</body>
+</html>
